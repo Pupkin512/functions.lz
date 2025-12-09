2de02107b7486a9d4e77cbe05af33638da0332f2
Pupkin512 <pupkin123@tutamail.com>
2025-12-09 23:32:23 +0300

    main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
     import is_even
     import is_prime
     import temperature
- 
-    if __name__ == '__main__':
+ def main():
         circle.main()  
         is_even.main()
         is_prime.main()
         temperature.main() 
-        
+
+ if __name__ == '__main__':
+     main()
 
 
 
